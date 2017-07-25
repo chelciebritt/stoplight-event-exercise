735c8125b66e8163f95538c5a4b5e634e3f1a362
Chelcie Britt <chelciebritt@Chelcies-MacBook-Pro.local>
2017-07-24 19:28:36 -0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-(function() {
-  'use strict';
+var myMouse = document.getElementById('controls');
 
-  // YOUR CODE HERE
-})();
+myMouse.onmouseover = function() {
+    console.log('entered button');
+}
+
+myMouse.onmouseout = function() {
+    console.log('left button');
+}
+const els =
+document.getElementById('stopButton').onclick = colorRed;
+document.getElementById('slowButton').onclick = colorYellow;
+document.getElementById('goButton').onclick = colorGreen;
+
+function colorRed() {
+  clearLights();
+  document.getElementById('stopLight').style.backgroundColor = "red";
+}
+function colorYellow() {
+  clearLights();
+  document.getElementById('slowLight').style.backgroundColor = "yellow";
+}
+function colorGreen() {
+  clearLights();
+  document.getElementById('goLight').style.backgroundColor = "green";
+}
+function clearLights() {
+  document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
+}
